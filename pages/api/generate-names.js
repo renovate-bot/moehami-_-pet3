641cf0c1d764ff2436cedbedf87a6c50e852fd69
moehami <m.aljaaly@yahoo.co.uk>
2024-11-12 03:19:57 +0200

    Update generate-names.js

--- pages/api/generate-names.js
+++ pages/api/generate-names.js
@@ -2,7 +2,7 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.openai
 });
 
 export default async function handler(req, res) {
