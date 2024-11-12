2a5202cba853e32bff94c03fcea71c6be5f7fc7a
moehami <m.aljaaly@yahoo.co.uk>
2024-11-12 15:39:08 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '@/styles/globals.css';
 import { specimen } from '@next/font/google'
 
 
-const specimen = Inter({ subsets: ['Josefin Sans'] })
+//const specimen = Inter({ subsets: ['Josefin Sans'] })
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
