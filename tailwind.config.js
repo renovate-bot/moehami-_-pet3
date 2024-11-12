f332546f273e4d3cd2e4b07667980b533a9ce79b
moehami <m.aljaaly@yahoo.co.uk>
2024-11-12 12:05:02 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+            './src/**/*.{js,jsx}',
+
   ],
   theme: {
   	fontFamily: {
