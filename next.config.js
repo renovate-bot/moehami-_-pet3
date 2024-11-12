c62a60294ffe26fec5656cddcd19f46880f4ca7c
moehami <m.aljaaly@yahoo.co.uk>
2024-11-12 12:37:18 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-webpack(config) {
+  webpack: (config) => {
+    // Customize the webpack configuration here
     config.module.rules.push({
       test: /\.css$/,
       use: ['style-loader', 'css-loader'],
     });
-
+    return config;
   },
+};
+
 module.exports = nextConfig;
